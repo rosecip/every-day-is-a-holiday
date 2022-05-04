3d6a1a843920802ed9c8b4869a49f0d06f2617ff
rosecip <rosecipriano10@gmail.com>
2022-05-04 16:03:21 -0400

    changed quotes for consistency

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { hot } from "react-hot-loader/root";
+import React, { useState, useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import { hot } from "react-hot-loader/root"
 
-import getCurrentUser from "../services/getCurrentUser";
-import "../assets/scss/main.scss";
-import RegistrationForm from "./registration/RegistrationForm";
-import SignInForm from "./authentication/SignInForm";
-import TopBar from "./layout/TopBar";
-import HolidayList from './HolidayList'
-import NewHolidayForm from "./NewHolidayForm";
+import getCurrentUser from "../services/getCurrentUser"
+import "../assets/scss/main.scss"
+import RegistrationForm from "./registration/RegistrationForm"
+import SignInForm from "./authentication/SignInForm"
+import TopBar from "./layout/TopBar"
+import HolidayList from "./HolidayList"
+import NewHolidayForm from "./NewHolidayForm"
 
 const App = (props) => {
   const [currentUser, setCurrentUser] = useState(undefined);
